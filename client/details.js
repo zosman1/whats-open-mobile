595d08847d94cb21fff9874d6d879d6875e9b25b
Zach Osman <zosman@gmu.edu>
2017-10-31 23:19:58 -0400

    Styling, Ui etc

--- client/details.js
+++ client/details.js
@@ -11,6 +11,7 @@ import {
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
 import { StackNavigator } from 'react-navigation';
 import Button from 'react-native-button';
+import MyText from 'react-native-letter-spacing';
 import { fetchData, isOpen, dayToString, milToStandard } from './data';
 
 
@@ -30,9 +31,13 @@ export class Details extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.facilityType}> {this.state.facility.facility_category.name} </Text>
+                <View style={{marginLeft: '2%'}}>
+                <Text style={styles.facilityType}>{this.state.facility.facility_category.name}</Text>
+                <MyText letterSpacing={1.4} style={styles.facilityLocation}>{this.state.facility.facility_location.building.toUpperCase()}</MyText>
+                </View> 
+
                 <TableView>
-                    <Section sectionTintColor='white' header='Hours'>
+                    <Section sectionTintColor='#F7F7F7' header='Hours'>
                         {
                             this.state.facility.main_schedule.open_times.map((schedule,i = 0) => {
                                 return (
@@ -58,13 +63,20 @@ export class Details extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: '#F7F7F7'
     },
     facilityType: {
-        fontSize: 30,
+        fontSize: 31,
         fontWeight: 'bold',
-        padding: 10
+        padding: 0,
+        color: '#282828'
         // textAlign: 'center',
+    },
+    facilityLocation: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        padding: 0,
+        color: '#75b4ff'
     },
     dayOfWeek: {
         fontSize: 20
