b598bcdc75786bbf56a7f8e7519983d307767ef9
Zach Osman <zosman@gmu.edu>
2017-12-24 10:53:37 -0500

    Commenting Functions in data.js

--- client/data.js
+++ client/data.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 import { isFacilityOpen } from './isOpen';
 
 export function sortFacilitys(facilitys){
+    // Sorts Facilities in a very stable manner
     let openFacilitys = [];
     let closedFacilitys = [];
 
@@ -14,11 +15,11 @@ export function sortFacilitys(facilitys){
     })
     Array.prototype.push.apply(openFacilitys, closedFacilitys);
     return openFacilitys;
-
 }
 
 
 export async function fetchData() {
+    // Fetches data from api 
     try {
         let response = await fetch('https://api.srct.gmu.edu/whatsopen/v2/facilities/?format=json')
         let responseJson = await response.json();
@@ -29,10 +30,15 @@ export async function fetchData() {
 }
 
 export function milToStandard(input){
+    // Takes in time in military format and returns it in standard format
     return moment(input, 'HH:mm:ss').format('h:mm:ss A');
 }
  
 export function dayToString(dayInt){
+    // Takes in a day as an int and returns the name of the day
+    // In this case 0 would be Monday as that is how Whats Open Api is formatted 
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     return days[dayInt];
 }
+
+ 
